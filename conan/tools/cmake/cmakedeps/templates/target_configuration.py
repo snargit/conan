4effbf05c5fdb586b1ce2504ad96ee3130a872ad
dbailey <dbailey@mathworks.com>
2023-01-26 17:33:22 +0000

    Make tag more things with the configuration

--- conan/tools/cmake/cmakedeps/templates/target_configuration.py
+++ conan/tools/cmake/cmakedeps/templates/target_configuration.py
@@ -53,16 +53,16 @@ class TargetConfigurationTemplate(CMakeDepsFileTemplate):
         set({{ pkg_name }}_FRAMEWORKS_FOUND{{ config_suffix }} "") # Will be filled later
         conan_find_apple_frameworks({{ pkg_name }}_FRAMEWORKS_FOUND{{ config_suffix }} "{{ '${' }}{{ pkg_name }}_FRAMEWORKS{{ config_suffix }}}" "{{ '${' }}{{ pkg_name }}_FRAMEWORK_DIRS{{ config_suffix }}}")
 
-        set({{ pkg_name }}_LIBRARIES_TARGETS "") # Will be filled later
+        set({{ pkg_name }}_LIBRARIES_TARGETS{{config_suffix}} "") # Will be filled later
 
 
         ######## Create an interface target to contain all the dependencies (frameworks, system and conan deps)
-        if(NOT TARGET {{ pkg_name+'_DEPS_TARGET'}})
-            add_library({{ pkg_name+'_DEPS_TARGET'}} INTERFACE IMPORTED)
+        if(NOT TARGET {{ pkg_name+'_DEPS_TARGET'+config_suffix}})
+            add_library({{ pkg_name+'_DEPS_TARGET'+config_suffix}} INTERFACE IMPORTED)
         endif()
 
-        set_property(TARGET {{ pkg_name + '_DEPS_TARGET'}} PROPERTY IMPORTED_CONFIGURATIONS {{ config }} APPEND)
-        set_property(TARGET {{ pkg_name + '_DEPS_TARGET'}}
+        set_property(TARGET {{ pkg_name + '_DEPS_TARGET'+config_suffix}} PROPERTY IMPORTED_CONFIGURATIONS {{ config }} APPEND)
+        set_property(TARGET {{ pkg_name + '_DEPS_TARGET'+config_suffix}}
                      PROPERTY INTERFACE_LINK_LIBRARIES
                      {{ '${'+pkg_name+'_FRAMEWORKS_FOUND'+config_suffix+'}' }}
                      {{ '${'+pkg_name+'_SYSTEM_LIBS'+config_suffix+'}' }}
@@ -70,14 +70,14 @@ class TargetConfigurationTemplate(CMakeDepsFileTemplate):
                      APPEND)
 
         ####### Find the libraries declared in cpp_info.libs, create an IMPORTED target for each one and link the
-        ####### {{pkg_name}}_DEPS_TARGET to all of them
+        ####### {{pkg_name}}_DEPS_TARGET{{config_suffix}} to all of them
         conan_package_library_targets("{{ '${' }}{{ pkg_name }}_LIBS{{ config_suffix }}}"    # libraries
                                       "{{ '${' }}{{ pkg_name }}_LIB_DIRS{{ config_suffix }}}" # package_libdir
                                       "{{ '${' }}{{ pkg_name }}_BIN_DIRS{{ config_suffix }}}" # package_bindir
                                       "{{ '${' }}{{ pkg_name }}_LIBRARY_TYPE{{ config_suffix }}}"
                                       "{{ '${' }}{{ pkg_name }}_IS_HOST_WINDOWS{{ config_suffix }}}"
-                                      {{ pkg_name + '_DEPS_TARGET'}}
-                                      {{ pkg_name }}_LIBRARIES_TARGETS  # out_libraries_targets
+                                      {{ pkg_name + '_DEPS_TARGET'+config_suffix}}
+                                      {{ pkg_name }}_LIBRARIES_TARGETS{{config_suffix}}  # out_libraries_targets
                                       "{{ config_suffix }}"
                                       "{{ pkg_name }}"    # package_name
                                       "{{ '${' }}{{ pkg_name }}_NO_SONAME_MODE{{ config_suffix }}}" # soname
@@ -93,7 +93,7 @@ class TargetConfigurationTemplate(CMakeDepsFileTemplate):
             set_property(TARGET {{root_target_name}}
                          PROPERTY INTERFACE_LINK_LIBRARIES
                          {{ '${'+pkg_name+'_OBJECTS'+config_suffix+'}' }}
-                         ${{'{'}}{{pkg_name}}_LIBRARIES_TARGETS}
+                         ${{'{'}}{{pkg_name}}_LIBRARIES_TARGETS{{config_suffix}}}
                          APPEND)
 
             if("{{ '${' }}{{ pkg_name }}_LIBS{{ config_suffix }}}" STREQUAL "")
@@ -102,7 +102,7 @@ class TargetConfigurationTemplate(CMakeDepsFileTemplate):
                 # global target
                 set_property(TARGET {{root_target_name}}
                              PROPERTY INTERFACE_LINK_LIBRARIES
-                             {{pkg_name}}_DEPS_TARGET
+                             {{pkg_name}}_DEPS_TARGET{{config_suffix}}
                              APPEND)
             endif()
 
@@ -140,15 +140,15 @@ class TargetConfigurationTemplate(CMakeDepsFileTemplate):
                 set({{ pkg_name }}_{{ comp_variable_name }}_FRAMEWORKS_FOUND{{ config_suffix }} "")
                 conan_find_apple_frameworks({{ pkg_name }}_{{ comp_variable_name }}_FRAMEWORKS_FOUND{{ config_suffix }} "{{ '${'+pkg_name+'_'+comp_variable_name+'_FRAMEWORKS'+config_suffix+'}' }}" "{{ '${'+pkg_name+'_'+comp_variable_name+'_FRAMEWORK_DIRS'+config_suffix+'}' }}")
 
-                set({{ pkg_name }}_{{ comp_variable_name }}_LIBRARIES_TARGETS "")
+                set({{ pkg_name }}_{{ comp_variable_name }}_LIBRARIES_TARGETS{{config_suffix}} "")
 
                 ######## Create an interface target to contain all the dependencies (frameworks, system and conan deps)
-                if(NOT TARGET {{ pkg_name + '_' + comp_variable_name + '_DEPS_TARGET'}})
-                    add_library({{ pkg_name + '_' + comp_variable_name + '_DEPS_TARGET'}} INTERFACE IMPORTED)
+                if(NOT TARGET {{ pkg_name + '_' + comp_variable_name + '_DEPS_TARGET'+config_suffix}})
+                    add_library({{ pkg_name + '_' + comp_variable_name + '_DEPS_TARGET'+config_suffix}} INTERFACE IMPORTED)
                 endif()
 
-                set_property(TARGET {{ pkg_name + '_' + comp_variable_name + '_DEPS_TARGET'}} PROPERTY IMPORTED_CONFIGURATIONS {{ config }} APPEND)
-                set_property(TARGET {{ pkg_name + '_' + comp_variable_name + '_DEPS_TARGET'}}
+                set_property(TARGET {{ pkg_name + '_' + comp_variable_name + '_DEPS_TARGET'+config_suffix}} PROPERTY IMPORTED_CONFIGURATIONS {{ config }} APPEND)
+                set_property(TARGET {{ pkg_name + '_' + comp_variable_name + '_DEPS_TARGET'+config_suffix}}
                              PROPERTY INTERFACE_LINK_LIBRARIES
                              {{ '${'+pkg_name+'_'+comp_variable_name+'_FRAMEWORKS_FOUND'+config_suffix+'}' }}
                              {{ '${'+pkg_name+'_'+comp_variable_name+'_SYSTEM_LIBS'+config_suffix+'}' }}
@@ -156,14 +156,14 @@ class TargetConfigurationTemplate(CMakeDepsFileTemplate):
                              APPEND)
 
                 ####### Find the libraries declared in cpp_info.component["xxx"].libs,
-                ####### create an IMPORTED target for each one and link the '{{pkg_name}}_{{comp_variable_name}}_DEPS_TARGET' to all of them
+                ####### create an IMPORTED target for each one and link the '{{pkg_name}}_{{comp_variable_name}}_DEPS_TARGET{{config_suffix}}' to all of them
                 conan_package_library_targets("{{ '${'+pkg_name+'_'+comp_variable_name+'_LIBS'+config_suffix+'}' }}"
                                       "{{ '${'+pkg_name+'_'+comp_variable_name+'_LIB_DIRS'+config_suffix+'}' }}"
                                       "{{ '${'+pkg_name+'_'+comp_variable_name+'_BIN_DIRS'+config_suffix+'}' }}" # package_bindir
                                       "{{ '${'+pkg_name+'_'+comp_variable_name+'_LIBRARY_TYPE'+config_suffix+'}' }}"
                                       "{{ '${'+pkg_name+'_'+comp_variable_name+'_IS_HOST_WINDOWS'+config_suffix+'}' }}"
-                                      {{ pkg_name + '_' + comp_variable_name + '_DEPS_TARGET'}}
-                                      {{ pkg_name }}_{{ comp_variable_name }}_LIBRARIES_TARGETS
+                                      {{ pkg_name + '_' + comp_variable_name + '_DEPS_TARGET'+config_suffix}}
+                                      {{ pkg_name }}_{{ comp_variable_name }}_LIBRARIES_TARGETS{{config_suffix}}
                                       "{{ config_suffix }}"
                                       "{{ pkg_name }}_{{ comp_variable_name }}"
                                       "{{ '${'+pkg_name+'_'+comp_variable_name+'_NO_SONAME_MODE'+config_suffix+'}' }}"
@@ -174,7 +174,7 @@ class TargetConfigurationTemplate(CMakeDepsFileTemplate):
                 set_property(TARGET {{comp_target_name}}
                              PROPERTY INTERFACE_LINK_LIBRARIES
                              {{ '${'+pkg_name+'_'+comp_variable_name+'_OBJECTS'+config_suffix+'}' }}
-                             ${{'{'}}{{pkg_name}}_{{comp_variable_name}}_LIBRARIES_TARGETS}
+                             ${{'{'}}{{pkg_name}}_{{comp_variable_name}}_LIBRARIES_TARGETS{{config_suffix}}}
                              APPEND)
 
                 if("{{ '${' }}{{ pkg_name }}_{{comp_variable_name}}_LIBS{{ config_suffix }}}" STREQUAL "")
@@ -182,7 +182,7 @@ class TargetConfigurationTemplate(CMakeDepsFileTemplate):
                     # linked to the imported targets and we need to do it to the global target
                     set_property(TARGET {{comp_target_name}}
                                  PROPERTY INTERFACE_LINK_LIBRARIES
-                                 {{pkg_name}}_{{comp_variable_name}}_DEPS_TARGET
+                                 {{pkg_name}}_{{comp_variable_name}}_DEPS_TARGET{{config_suffix}}
                                  APPEND)
                 endif()
 
