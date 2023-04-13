faaeef0f2127f86fb0a49558eb675f4427c25138
dbailey <dbailey@mathworks.com>
2023-04-13 16:10:59 +0100

    line endings and no duplicate upstream target libraries

--- conan/tools/cmake/cmakedeps/templates/target_configuration.py
+++ conan/tools/cmake/cmakedeps/templates/target_configuration.py
@@ -93,8 +93,8 @@ class TargetConfigurationTemplate(CMakeDepsFileTemplate):
             set_property(TARGET {{root_target_name}} PROPERTY IMPORTED_CONFIGURATIONS {{ config }} APPEND)
             set_property(TARGET {{root_target_name}}
                          PROPERTY INTERFACE_LINK_LIBRARIES
-                         $<$<CONFIG:{{configuration}}>:{{ '${'+pkg_name+'_OBJECTS'+config_suffix+'}' }}
-                                    ${{'{'}}{{pkg_name}}_LIBRARIES_TARGETS{{config_suffix}}}>
+                         $<$<CONFIG:{{configuration}}>:{{ '${'+pkg_name+'_OBJECTS'+config_suffix+'}' }}>
+                         ${{'{'}}{{pkg_name}}_LIBRARIES_TARGETS{{config_suffix}}}
                          APPEND)
 
             if("{{ '${' }}{{ pkg_name }}_LIBS{{ config_suffix }}}" STREQUAL "")
@@ -178,8 +178,8 @@ class TargetConfigurationTemplate(CMakeDepsFileTemplate):
                 ########## TARGET PROPERTIES #####################################
                 set_property(TARGET {{comp_target_name}}
                              PROPERTY INTERFACE_LINK_LIBRARIES
-                             $<$<CONFIG:{{configuration}}>:{{ '${'+pkg_name+'_'+comp_variable_name+'_OBJECTS'+config_suffix+'}' }}
-                                        ${{'{'}}{{pkg_name}}_{{comp_variable_name}}_LIBRARIES_TARGETS{{config_suffix}}}>
+                             $<$<CONFIG:{{configuration}}>:{{ '${'+pkg_name+'_'+comp_variable_name+'_OBJECTS'+config_suffix+'}' }}>
+                             ${{'{'}}{{pkg_name}}_{{comp_variable_name}}_LIBRARIES_TARGETS{{config_suffix}}}
                              APPEND)
 
                 if("{{ '${' }}{{ pkg_name }}_{{comp_variable_name}}_LIBS{{ config_suffix }}}" STREQUAL "")
